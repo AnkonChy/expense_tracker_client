4d5dff7eee64d8d90ed6af490fba21336f3d8234
Ankon Chowdhury <ankonchy1@gmail.com>
2025-07-05 11:40:28 +0600

    all setup & configuration done

---
+++ src/pages/AddExpense/AddExpense.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddExpense = () => {
+    return (
+        <div>
+            <h1>I am add expense</h1>
+        </div>
+    );
+};
+
+export default AddExpense;
